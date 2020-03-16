a32da5e27b9f8f411e2f56b4fd5c8da85bb80e1e
Kharoh <Gaulu03@gmail.com>
2020-03-16 16:46:12 +0100

    event handler and kf-database

---
+++ assets/assets.js
@@ -0,0 +1,7 @@
+const config = require('./global/config')
+
+const assets = {
+    config
+}
+
+module.exports = assets
--- index.js
+++ index.js
@@ -1,3 +1,39 @@
-const Discord = require('discord')
+/* global imports */
+const fs = require('fs')
+const Discord = require('discord.js')
+const Base = require('kf-database')
+require('dotenv').config()
+const assets = require('./assets/assets')
+
+/* subglobal imports */
+const config = assets.config
+const client = new Discord.Client()
+const db = new Base('')
+
+/* event handler */
+fs.readdir('./src/events', (err, files) => {
+    if (err) throw err
+    files.forEach(fileName => {
+        if (!fileName.endsWith('.js')) return
+        const event = require(`./src/events/${fileName}`)
+        const eventName = fileName.split('.')[0]
+        client.on(eventName, event.bind(null, client))
+        delete require.cache[require.resolve(`./src/events/${fileName}`)]
+    })
+})
 
 
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!')
+  }
+})
+
+
+/* log the client in */
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.login(process.env.CLIENT_TOKEN)
